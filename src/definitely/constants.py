7d91c60678ea33ad23445f2a6ddc3ccac9011363
Daniel Lindsley <daniel@toastdriven.com>
2022-06-15 00:53:07 -0500

    Comments & docs strings for constants/exceptions.

--- src/definitely/constants.py
+++ src/definitely/constants.py
@@ -1,3 +1,5 @@
+# A sentinel for when a subclass has not defined a default state.
 UNKNOWN_STATE = "unknown"
 
+# A constant to represent any/all state names.
 ANY = "any"
--- src/definitely/exceptions.py
+++ src/definitely/exceptions.py
@@ -1,22 +1,46 @@
 class DefinitelyAnError(Exception):
+    """
+    A base exception for the others to inherit from.
+    """
+
     pass
 
 
 class InvalidState(DefinitelyAnError):
+    """
+    Raised when an unknown state is attempted to be transitioned to.
+    """
+
     pass
 
 
 class TransitionNotAllowed(DefinitelyAnError):
+    """
+    Raised when the provide state is not allowed to be transitioned to.
+    """
+
     pass
 
 
 class InvalidHandler(DefinitelyAnError):
+    """
+    Raised when a `handle_*` attribute is present, but can not be called.
+    """
+
     pass
 
 
 class InvalidDefaultState(DefinitelyAnError):
+    """
+    Raised when no default state has been specified on a FSM subclass.
+    """
+
     pass
 
 
 class NoStatesDefined(DefinitelyAnError):
+    """
+    Raised when no states/transitions have been specified on a FSM subclass.
+    """
+
     pass
